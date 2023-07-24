677e35cd7aef55afa7b25e89caf2ee0741499512
sscode02 <1227918543@qq.com>
2023-07-25 00:46:39 +0800

    feat: add createRouteRecordMatcher

--- router/router.ts
+++ router/router.ts
@@ -16,7 +16,7 @@ export type PathParseOptions = {
 
 
 export function createRouter(options: RouterOptions) {
-    const matcher = createRouterMatcher(options.routers, options)
+    const matcher = createRouterMatcher(options.routers, options as PathParseOptions)
 
     const install = () => {
 
@@ -35,6 +35,53 @@ export function createRouterMatcher(routers: Readonly<routeRecordRaw[]>, globalO
         { end: true, strict: false, sensitive: false } as PathParseOptions,
         globalOptions
     )
+
+    function addRoute(record: any) {
+        const mainNormalizedRecord = normalizeRouteRecord(record)
+
+        const options = mergeOptions(globalOptions, record)
+
+        const normalizRecords = [mainNormalizedRecord]
+
+        let originalMatcher
+        let matcher
+
+        for (const normalizedRecord of normalizRecords) {
+            matcher = createRouteRecordMatcher(normalizedRecord, options)
+            originalMatcher = originalMatcher
+            inserMatcher(matcher)
+        }
+    }
+
+    function inserMatcher(matcher: any) {
+        matchers.push(matcher)
+    }
+
+    routers.forEach(route => addRoute(route))
+}
+
+
+function createRouteRecordMatcher(record: any, options: any) {
+    const parser = {
+        re: undefined,
+        score: undefined,
+        keys: undefined,
+        parse: () => { },
+        stringity: () => { }
+    }
+
+    const matcher = Object.assign(parser, { record, children: [], alias: [] })
+    return matcher
+
+}
+
+
+
+function normalizeRouteRecord(record: any) {
+    return {
+        path: record.path,
+        components: 'componets' in record ? record.components || null : record.component && { default: record.component }
+    }
 }
 
 function mergeOptions<T extends object>(defaults: T, partialOptions: Partial<T>): T {
