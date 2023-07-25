ec819863ef625777498a9154e5e2b7e81169e5c4
sscode02 <1227918543@qq.com>
2023-07-25 13:49:12 +0800

    feat: final matcher

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { router } from './router/router'
+import ComponentB from './components/ComponentB.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('ComponentB', ComponentB) // TODO: pr mount API should async
+app.use(router).mount('#app') 
